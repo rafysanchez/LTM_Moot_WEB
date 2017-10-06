38a53ae8584dc47dcb017644757f35caaf603560
rafael sanchez port <rafysanchez@hotmail.com>
2017-10-05 23:24:55 -0300

    token angulars front end
    sistema com Angulars e WebApi com Identity

---
+++ Token_Example_Angulars/AngularJSAuthentication.API/Controllers/OrdersController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Web.Http;
+
+namespace AngularJSAuthentication.API.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            //ClaimsPrincipal principal = Request.GetRequestContext().Principal as ClaimsPrincipal;
+
+            //var Name = ClaimsPrincipal.Current.Identity.Name;
+            //var Name1 = User.Identity.Name;
+
+            //var userName = principal.Claims.Where(c => c.Type == "sub").Single().Value;
+
+            return Ok(Order.Listar());
+        }
+
+    }
+
+
+    #region Helpers
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerName { get; set; }
+        public string ShipperCity { get; set; }
+        public Boolean IsShipped { get; set; }
+
+
+        public static List<Order> Listar()
+        {
+            List<Order> OrderList = new List<Order> 
+            {
+                new Order {OrderID = 001, CustomerName = "Adriana Lima", ShipperCity = "Santos", IsShipped = true },
+                new Order {OrderID = 002, CustomerName = "Gisele Bunchen", ShipperCity = "São Paulo", IsShipped = false},
+                new Order {OrderID = 003,CustomerName = "Roberto Lima", ShipperCity = "Rio de Janeiro", IsShipped = false },
+                new Order {OrderID = 004,CustomerName = "Acacio Mendes", ShipperCity = "Limeira", IsShipped = false},
+                new Order {OrderID = 005,CustomerName = "Rogeria Ramula", ShipperCity = "Campinas", IsShipped = true}
+            };
+
+            return OrderList;
+        }
+    }
+
+    #endregion
+}
---
+++ Token_Example_Angulars/AngularJSAuthentication.API/Models/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AngularJSAuthentication.API.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "A {0} deve ser ao menos {2} caracteres de comprimento.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Senha e confirmação não são iguais.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+   
+}
